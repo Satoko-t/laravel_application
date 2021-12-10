bbb7c3b1dd6bba32c08788869ac0d8da881e9003
Satoko-t <stkuoc013105@gmail.com>
2021-12-11 00:26:45 +0900

    laravel課題7完了したが投稿できない

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'PostController@index')->name('post.index');
+Route::group(['middleware' => 'auth'], function() {
+    Route::get('post/create', 'PostController@create')->name('post.create');
+    Route::post('post/create', 'PostController@store')->name('post.store');
+});
